03fe95c4678dd63a8624a5689465e384507f9b53
VILLAN3LL3 <development@gutfrau.de>
2023-09-06 17:19:50 +0200

    use createactorcontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React, { createContext, useContext } from 'react';
-import { useActor, useInterpret } from '@xstate/react';
+import React from 'react';
+import { createActorContext } from '@xstate/react';
 import { machine } from './Machine';
 
-export const AuthActorContext = createContext({});
-
-export const GlobalStateProvider = (props) => {
-  const authActor = useInterpret(machine); // uses MachineContext, but not MachineEvent
-
-  return (
-    <AuthActorContext.Provider value={authActor}>
-      {props.children}
-    </AuthActorContext.Provider>
-  );
-};
-
-
+export const Context = createActorContext(machine);
 
 export const App = () => {
-  const authActor = useContext(AuthActorContext);
-  // compiler error "no overload matches this call"
-  const [state, send] = useActor(authActor); // type of state is any, type of Sender is any => no type safety
-
-  return state.matches('loggedIn') ? 'Logged In' : 'Logged Out';
+  <Context.Provider options={{ context: { value: "initial value" }}}>
+    <ChildComponent />
+  </Context.Provider>
 };
+
+export function ChildComponent() {
+  const [state, send] = Context.useActor();
+
+  send("BAR");
+  return state.matches("a") ? 'Logged In' : 'Logged Out';
+}
